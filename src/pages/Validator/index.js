2a9953ba303ea1a9ab63bc6420c87a3081bcc21a
Matthew Rosendin <matthew.rosendin@gmail.com>
2021-09-09 20:43:22 -0700

    improve payment form

--- src/pages/Validator/index.js
+++ src/pages/Validator/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { Button, Form } from "react-bootstrap"
+import { Button, Form, Row, Col } from "react-bootstrap"
 import { updateEdges } from "../../graph"
 import { api } from "../../xrpl";
 
@@ -20,6 +20,44 @@ function Validator() {
   const [sendMaxIssuer, setSendMaxIssuer] = React.useState("")
   const [destination, setDestination] = React.useState("")
   const accounts = JSON.parse(localStorage.getItem("accounts"))
+  const [paths, setPaths] = React.useState()
+
+  React.useEffect(() => {
+    const WEBSOCKET_SERVER = "wss://s.altnet.rippletest.net:51233/"
+    const ws = new WebSocket(WEBSOCKET_SERVER)
+
+    ws.onopen = () => {
+      if (account && destination && destinationIssuer)
+      ws.send(JSON.stringify({
+        command: "path_find",
+        destination_account: accounts[destination].account.address,
+        destination_amount: {
+          value: destinationValue,
+          currency: destinationCurrency,
+          issuer: accounts[destinationIssuer].account.address
+        },
+        currency: "AUR",
+        issuer: accounts[destinationIssuer].account.address,
+        value: "1",
+        id: 8,
+        source_account: accounts[account].account.address,
+        subcommand: "create"
+      }))
+    }
+
+    ws.onmessage = event => {
+      const message = JSON.parse(event.data)
+      setPaths(message)
+    }
+
+    ws.onclose = () => {
+      console.log(`Disconnected from ${WEBSOCKET_SERVER}`)
+    }
+
+    return () => {
+      ws.close()
+    }
+  }, [])
 
   const submit = async () => {
     setLoading(true)
@@ -64,56 +102,82 @@ function Validator() {
     setDestinationIssuer(value)
   }
 
+  const onChangeSendMaxValue = async (event) => {
+    const value = event.target.value
+    setSendMaxValue(value)
+  }
+
+  const onChangeSendMaxCurrency = async (event) => {
+    const value = event.target.value
+    setSendMaxCurrency(value)
+  }
+
+  const onChangeSendMaxIssuer = async (event) => {
+    const value = event.target.value
+    setSendMaxIssuer(value)
+  }
+
   return (
-    <div className="container-fluid mt-5">
+    <div className="container mt-5">
       <div className="row">
-        <div className="col-4 offset-4">
-          <h4>Validator</h4>
+        <div className="col-6">
+          <h4>Payment Form</h4>
+          <p className="text-muted border rounded p-3">
+            <span className="fa fa-info-circle" /> Use node IDs instead of account addresses.
+          </p>
           <Form>
-            <Form.Group className="mb-3">
-              <Form.Label>Account</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={onChangeAccount}
-                defaultValue={account}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Destination</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={onChangeDestination}
-                defaultValue={destination}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Destination Amount</Form.Label>
-              <Form.Control
-                type="number"
-                onChange={onChangeDestinationAmount}
-                defaultValue={destinationValue}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Destination Currency</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={onChangeDestinationCurrency}
-                defaultValue={destinationCurrency}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3">
-              <Form.Label>Destination Issuer</Form.Label>
-              <Form.Control
-                type="text"
-                onChange={onChangeDestinationIssuer}
-                defaultValue={destinationIssuer}
-              />
-            </Form.Group>
+            <h4>Source</h4>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Source Account</Form.Label>
+                <Form.Control type="text" onChange={onChangeAccount} defaultValue={account} />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Max Amount</Form.Label>
+                <Form.Control type="number" onChange={onChangeSendMaxValue} defaultValue={sendMaxValue} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Currency</Form.Label>
+                <Form.Control type="text" onChange={onChangeSendMaxCurrency} defaultValue={sendMaxCurrency} />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Issuer</Form.Label>
+                <Form.Control type="number" onChange={onChangeSendMaxIssuer} defaultValue={sendMaxIssuer} />
+              </Form.Group>
+            </Row>
+            <h4>Destination</h4>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Destination Account</Form.Label>
+                <Form.Control type="text" onChange={onChangeDestination} defaultValue={destination} />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Destination Amount</Form.Label>
+                <Form.Control type="number" onChange={onChangeDestinationAmount} defaultValue={destinationValue} />
+              </Form.Group>
+            </Row>
+            <Row className="mb-3">
+              <Form.Group as={Col}>
+                <Form.Label>Destination Currency</Form.Label>
+                <Form.Control type="text" onChange={onChangeDestinationCurrency} defaultValue={destinationCurrency} />
+              </Form.Group>
+              <Form.Group as={Col}>
+                <Form.Label>Destination Issuer</Form.Label>
+                <Form.Control type="number" onChange={onChangeDestinationIssuer} defaultValue={destinationIssuer} />
+              </Form.Group>
+            </Row>
           </Form>
           <Button variant="primary" onClick={submit} disabled={loading}>
             { loading ? <i className="fas fa-spin fa-spinner" /> : "Submit" }
           </Button>
+        </div>
+        <div className="col-6">
+          <h4>Valid Paths</h4>
+          <pre className="bg-light p-5">
+            {JSON.stringify(paths, null, 2)}
+          </pre>
         </div>
       </div>
     </div>
